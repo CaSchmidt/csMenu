a8ac568e32f88c335691f9f73ac75d4274d6fe3f
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2023-06-25 20:25:05 +0200

    Test bench for the Win32Compat library.

---
+++ csMenu3/tests/src/test_win32.cpp
@@ -0,0 +1,59 @@
+#include <thread>
+
+#define NOMINMAX
+#include <Windows.h>
+
+#include "Win32/Message.h"
+#include "Win32/ProgressBar.h"
+
+void thread_work(const int i, ProgressBar *pb)
+{
+  Sleep(i * 1000);
+  pb->step();
+}
+
+struct Dispatch {
+  Dispatch() noexcept = delete;
+
+  Dispatch(const int numItems, ProgressBar *pb) noexcept
+    : i{0}
+    , numItems{numItems}
+    , pb{pb}
+  {
+  }
+
+  void operator()(const UINT_t message)
+  {
+    if( message != ProgressBar::getStepItMessage() ) {
+      return;
+    }
+
+    if( i < numItems ) {
+      i += 1;
+      std::thread{thread_work, i, pb}.detach();
+    } else {
+      message::postQuit(0);
+    }
+  }
+
+  int i{0};
+  int numItems{0};
+  ProgressBar *pb{nullptr};
+};
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+  constexpr int MIN_ITEM  = 0;
+  constexpr int NUM_ITEMS = 4;
+
+  ProgressBarPtr pb = ProgressBar::make(hInstance, 480, 48);
+  pb->setRange(MIN_ITEM, MIN_ITEM + NUM_ITEMS);
+  pb->show();
+
+  Dispatch dispatch{NUM_ITEMS, pb.get()};
+  dispatch(ProgressBar::getStepItMessage());
+
+  message::loop(dispatch);
+
+  return 0;
+}
