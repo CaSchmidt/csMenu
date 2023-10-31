9201da94f1f021f041d9a813b56f3fa1d6a49622
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2023-10-31 17:56:07 +0100

    Modularize context menu factory.

---
+++ csMenu3/src/MenuFactory.cpp
@@ -0,0 +1,96 @@
+/****************************************************************************
+** Copyright (c) 2023, Carsten Schmidt. All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+**
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+**
+** 3. Neither the name of the copyright holder nor the names of its
+**    contributors may be used to endorse or promote products derived from
+**    this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#include "MenuFactory.h"
+
+#include "CommandEnum.h"
+#include "CommandFlag.h"
+#include "CommandInvoke.h"
+#include "CommandSeparator.h"
+#include "MenuFlags.h"
+
+////// Private ///////////////////////////////////////////////////////////////
+
+namespace impl_menu {
+
+  void buildContextMenu(CommandEnum *menu)
+  {
+    if( menu == nullptr ) {
+      return;
+    }
+
+    menu->append(winrt::make<CommandInvoke>(Command::List));
+    menu->append(winrt::make<CommandInvoke>(Command::ListPath));
+    menu->append(winrt::make<CommandInvoke>(Command::ListPathTabular));
+
+    // Settings ////////////////////////////////////////////////////////////////
+
+    menu->append(winrt::make<CommandSeparator>());
+
+    const MenuFlags flags = readFlags();
+    menu->append(winrt::make<CommandFlag>(flags.testAny(MenuFlag::BatchProcessing), Command::CheckBatchProcessing));
+    menu->append(winrt::make<CommandFlag>(flags.testAny(MenuFlag::ParallelExecution), Command::CheckParallelExecution));
+    menu->append(winrt::make<CommandFlag>(flags.testAny(MenuFlag::ResolveUncPaths), Command::CheckResolveUncPaths));
+    menu->append(winrt::make<CommandFlag>(flags.testAny(MenuFlag::UnixPathSeparators), Command::CheckUnixPathSeparators));
+  }
+
+} // namespace impl_menu
+
+////// public ////////////////////////////////////////////////////////////////
+
+IFACEMETHODIMP MenuFactory::CreateInstance(IUnknown *pUnkOuter, REFIID riid, void **ppvObject)
+{
+  if( pUnkOuter != nullptr ) {
+    return CLASS_E_NOAGGREGATION;
+  }
+
+  try {
+    auto root = winrt::make<CommandEnum>(Command::RootMenu);
+    impl_menu::buildContextMenu(dynamic_cast<CommandEnum *>(root.get()));
+
+    return root->QueryInterface(riid, ppvObject);
+  } catch( ... ) {
+    return winrt::to_hresult();
+  }
+
+  return E_NOINTERFACE;
+}
+
+IFACEMETHODIMP MenuFactory::LockServer(BOOL fLock)
+{
+  if( fLock ) {
+    ++winrt::get_module_lock();
+  } else {
+    --winrt::get_module_lock();
+  }
+
+  return S_OK;
+}
