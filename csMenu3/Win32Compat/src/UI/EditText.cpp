661f4491a2c4d628cc00c5fcb2bafa05bd6b5ee9
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2024-10-06 18:09:00 +0200

    Add Win32 edit text.

---
+++ csMenu3/Win32Compat/src/UI/EditText.cpp
@@ -0,0 +1,91 @@
+/****************************************************************************
+** Copyright (c) 2024, Carsten Schmidt. All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+**
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+**
+** 3. Neither the name of the copyright holder nor the names of its
+**    contributors may be used to endorse or promote products derived from
+**    this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#include <Windows.h>
+
+#include "Win32/UI/EditText.h"
+
+#include "Win32/WindowUtil.h"
+
+namespace ui {
+
+  ////// public //////////////////////////////////////////////////////////////
+
+  EditText::EditText(HWND_t wnd, const ctor_tag&) noexcept
+    : Window(wnd)
+  {
+  }
+
+  EditText::EditText(HWND_t dlg, int idDlgItem, const ctor_tag&) noexcept
+    : Window(dlg, idDlgItem)
+  {
+  }
+
+  EditText::~EditText() noexcept
+  {
+  }
+
+  void EditText::setPasswordChar(const wchar_t ch)
+  {
+    sendMessage(EM_SETPASSWORDCHAR, ch, 0);
+  }
+
+  bool EditText::setReadOnly(const bool on)
+  {
+    const WPARAM wParam = on
+                          ? TRUE
+                          : FALSE;
+    return sendMessage(EM_SETREADONLY, wParam, 0) != LRESULT_t{0};
+  }
+
+  std::wstring EditText::text() const
+  {
+    return window::text(handle());
+  }
+
+  bool EditText::setText(const std::wstring& text)
+  {
+    return window::setText(handle(), text.data());
+  }
+
+  ////// public static /////////////////////////////////////////////////////
+
+  WindowPtr EditText::create(HWND_t wnd)
+  {
+    return std::make_unique<EditText>(wnd);
+  }
+
+  WindowPtr EditText::create(HWND_t dlg, int idDlgItem)
+  {
+    return std::make_unique<EditText>(dlg, idDlgItem);
+  }
+
+} // namespace ui
